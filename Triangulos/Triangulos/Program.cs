4a6d209caaffb8aec7158a5b6575a00cfe89fb20
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2021-02-28 16:28:50 -0300

    Update Program.cs

--- Triangulos/Triangulos/Program.cs
+++ Triangulos/Triangulos/Program.cs
@@ -6,27 +6,27 @@ namespace Triangulos
     {
         static void Main(string[] args)
         {
-            for (int i = 9; i >= 0; i--)
+            /*for (int i = 9; i >= 0; i--)
             {
                 for (int K = 9; K >= 0; K--) //colocar  i ao invés do 9, para formar um triângulo
                 {
                     Console.Write(K + " ");
                 }
                 Console.WriteLine();
-            }
-            // // Triângulo com o while
-            // int i = 9;
-            // while (i >= 0)
-            // {
-            //     int k = i;
-            //     while (k >= 0)
-            //     {
-            //         Console.Write(k + " ");
-            //         k--;
-            //     }
-            //     Console.WriteLine();
-            //     i--;
-            // }
+            }*/
+             //  Triângulo com o while
+                 int i = 9;
+                 while (i >= 0)
+                 {
+                     int k = i;
+                     while (k >= 0)
+                     {
+                         Console.Write(k + " ");
+                         k--;
+                     }
+                     Console.WriteLine();
+                     i--;
+             }
             //// Triângulo com o for
             // for (int i = 9; i >= 0; i--)
             //     {
