50d92e43aa8f192bf05fa690f75d508059d4f40b
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2021-03-02 16:04:41 -0300

    Add files via upload

---
+++ ListarNomesDaLista/ListarNomesDaLista/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListarNomesDaLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nomes = new List<string>() { "Ana", "Beatriz", "Carla", "Evelyn", "Fátima" };
+
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
