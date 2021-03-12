ae13c606f2a137e470e20346e18dd67b63d5dbe6
Matheus Henrique <62816022+Matheus-bot@users.noreply.github.com>
2021-03-12 13:06:35 -0300

    Add files via upload

---
+++ RestApiModeloDDD/RestApiModeloDDD.Infrastructure/Data/SqlContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestApiModeloDDD.Infrastructure.Data
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext()
+        {
+
+        }
+
+        public SqlContext(DbContextOptions<SqlContext>  options) : base(options)
+        {
+
+        }
+        public DbSet<Cliente> clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(Entry => Entry.Entry.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+            }
+            return base.SaveChanges();
+        
+        }
+    }
+}
